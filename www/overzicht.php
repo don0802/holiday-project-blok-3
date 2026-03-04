f65e80ac2adf677f1b5e735a706efb93fb738566
Don <don.mathurot@hotmail.com>
2026-03-04 09:59:49 +0100

    Use mysqli_fetch_all and foreach for destinations
    Fetch all destination rows into $constructors with mysqli_fetch_all and iterate with foreach ($constructor) instead of streaming mysqli_fetch_assoc. Renames template variables from $bestemming to $constructor and updates image, title, meta and link usages accordingly to avoid repeated result-pointer issues and improve readability.

--- www/overzicht.php
+++ www/overzicht.php
@@ -3,6 +3,7 @@ include 'database.php';
 
 $query = "SELECT * FROM destinations";
 $result = mysqli_query($conn, $query);
+$constructors = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -63,23 +64,23 @@ $result = mysqli_query($conn, $query);
     <button id="zoekButton" style="display: none;">Zoeken</button>
 
     <div class="destinations">
-        <?php while ($bestemming = mysqli_fetch_assoc($result)) { ?>
-            <a class="id" href="detailpagina.php?id=<?php echo $bestemming['id']; ?>">
+        <?php foreach ($constructors as $constructor): ?>
+            <a class="id" href="detailpagina.php?id=<?php echo $constructor['id']; ?>">
                 <div class="destination-card">
-                    <img src="https://picsum.photos/seed/<?php echo $bestemming['thumbnail_name']; ?>/300/200">
+                    <img src="https://picsum.photos/seed/<?php echo $constructor['thumbnail_name']; ?>/300/200">
                     <div class="destination-card-content">
-                        <h2><?php echo $bestemming['naam']; ?></h2>
-                        <p><?php echo $bestemming['beschrijving']; ?></p>
+                        <h2><?php echo $constructor['naam']; ?></h2>
+                        <p><?php echo $constructor['beschrijving']; ?></p>
                         <div class="meta">
-                            <span class="continent"><?php echo $bestemming['continent']; ?></span>
-                            <span class="land"><?php echo $bestemming['land']; ?></span>
-                            <span class="vakantietype"><?php echo $bestemming['vakantietype']; ?></span>
-                            <span class="prijs">€<?php echo $bestemming['prijs']; ?></span>
+                            <span class="continent"><?php echo $constructor['continent']; ?></span>
+                            <span class="land"><?php echo $constructor['land']; ?></span>
+                            <span class="vakantietype"><?php echo $constructor['vakantietype']; ?></span>
+                            <span class="prijs">€<?php echo $constructor['prijs']; ?></span>
                         </div>
                     </div>
                 </div>
             </a>
-        <?php } ?>
+        <?php endforeach; ?>
     </div>
 </main>
 
